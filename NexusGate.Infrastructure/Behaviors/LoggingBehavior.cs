dfa10eb5e725045149b111e1c1c5c58c839531b9
enricoroselino <enricoroselino@gmail.com>
2024-10-07 13:51:11 +0700

    added LoggingBehavior.cs

--- NexusGate.Infrastructure/Behaviors/LoggingBehavior.cs
+++ NexusGate.Infrastructure/Behaviors/LoggingBehavior.cs
@@ -3,22 +3,23 @@ using Microsoft.Extensions.Logging;
 
 namespace NexusGate.Infrastructure.Behaviors;
 
-internal sealed class LoggingBehavior<TRequest, TResponse>(
-    ILogger<LoggingBehavior<TRequest, TResponse>> logger)
-    : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : notnull, IRequest<TResponse>
-    where TResponse : notnull
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
 {
-    public async Task<TResponse> Handle(TRequest request,
-        RequestHandlerDelegate<TResponse> next,
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
-        logger.LogInformation("[START] Handle Request={Request} - Response={Response} - RequestData={RequestData}",
+        _logger.LogInformation("[START] Handle Request={Request} - Response={Response} - RequestData={RequestData}",
             typeof(TRequest).Name, typeof(TResponse).Name, request);
-
         var response = await next();
-
-        logger.LogInformation("[END] Handled {Request} with {Response}",
+        _logger.LogInformation("[END] Handled {Request} with {Response}",
             typeof(TRequest).Name, typeof(TResponse).Name);
 
         return response;
