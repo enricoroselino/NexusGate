c8b16e7e745ff69d28dea1ebc5a141f10f7f3f40
enricoroselino <enricoroselino@gmail.com>
2024-10-07 11:27:24 +0700

    remove register service in WeatherModule

--- NexusGate/Modules/Weather/WeatherModule.cs
+++ NexusGate/Modules/Weather/WeatherModule.cs
@@ -7,17 +7,11 @@ public class WeatherModule : IModule
     public IServiceCollection RegisterModule(IServiceCollection builder)
     {
         RegisterRepositories(builder);
-        RegisterServices(builder);
         return builder;
     }
 
     private static void RegisterRepositories(IServiceCollection services)
     {
         services.AddScoped<IWeatherRepository, WeatherRepository>();
-    }
-
-    private static void RegisterServices(IServiceCollection services)
-    {
-        
     }
 }
